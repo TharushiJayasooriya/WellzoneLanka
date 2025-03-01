a369d96f58174153338d1cb96064ebe7e450f2be
Chathuni Sooriyaarachchi <chathuni.20230146@iit.ac.lk>
2025-03-02 01:40:07 +0530

    update mongodb.ts
    enviroment variable error fix

--- frontend/src/lib/mongodb.ts
+++ frontend/src/lib/mongodb.ts
@@ -1,18 +1,26 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from 'mongoose';
 
-const MONGODB_URI=process.env.MONGO;
-if (!MONGODB_URI){
-    throw new Error("Please define mongo environment variable")
+const MONGODB_URI = process.env.MONGO;
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGO environment variable");
 }
 
 async function connectToDatabase() {
-    if(mongoose.connection.readyState===1){
-        return mongoose;
-    }
-    const opts={
-        bufferCommands:false,
-    }
-    await mongoose.connect(MONGODB_URI!,opts);
+  if (mongoose.connection.readyState === 1) {
+    console.log("Already connected to MongoDB");
     return mongoose;
+  }
+
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+    console.log("Connected to MongoDB");
+    return mongoose;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
 }
+
 export default connectToDatabase;
