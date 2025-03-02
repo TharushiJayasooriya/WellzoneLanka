0def64ddedbf22fe20e7846c4681954e49c47c15
Chathuni Sooriyaarachchi <chathuni.20230146@iit.ac.lk>
2025-03-02 17:08:23 +0530

    update user.ts
    fix user file cuz got error in authorize function

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -1,11 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose, { Document,Model, Schema } from "mongoose";
 
-const userSchema = new mongoose.Schema({
+interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+const userSchema :Schema<IUser>=new mongoose.Schema ({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User : Model<IUser>=mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
 export default User;
