7a56fdb6b46170ecf31fbf0cebf81439d457303d
Esitha05834 <esithajayasundara@gmail.com>
2025-03-03 21:29:06 +0530

    updated  about page
    made some space on top of the page (Transforming Wellness in Sri Lanka) added margin top

--- frontend/src/app/about/page.jsx
+++ frontend/src/app/about/page.jsx
@@ -73,7 +73,7 @@ export default function About() {
           className="absolute inset-0 flex items-center justify-center text-center px-4 sm:px-6 lg:px-8"
         >
           <div className="max-w-4xl">
-            <h1 className="text-5xl md:text-6xl font-bold text-white mb-6 leading-tight">
+            <h1 className="mt-12 text-5xl md:text-6xl font-bold text-white mb-6 leading-tight">
               Transforming <span className="text-blue-300">Wellness</span> in Sri Lanka
             </h1>
             <p className="text-xl md:text-2xl text-white/90 mb-8">
