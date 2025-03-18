4d679480bfca36089542cc84d29807fcea9aa676
Chathuni Sooriyaarachchi <chathuni.20230146@iit.ac.lk>
2025-03-18 22:06:13 +0530

    reset passwords- api s

---
+++ frontend/src/app/api/auth/reset-password/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import User from "../../../../models/user";
+import connectToDatabase from "../../../../lib/mongodb";
+import bcrypt from "bcryptjs"
+
+export const POST = async (request: any) => {
+ 
+    const { password,email } = await request.json();
+
+    await connectToDatabase();
+
+    const existingUser=await User.findOne((email));
+
+    const hashedPassword= await bcrypt.hash(password,5);
+    existingUser.password=hashedPassword;
+
+    existingUser.resetToken=undefined;
+    existingUser.resetTokenExpiry=undefined;
+
+    try{
+        await existingUser.save();
+        return new NextResponse("User's password is updated",{status:200});
+    }catch(error:any){
+        return new NextResponse(error,{status:500});
+    }
+}
