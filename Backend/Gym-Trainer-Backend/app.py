4249f4ad19cabfd614d7de309c147d34da8866a1
Disath Damsana <122661147+Disath2005@users.noreply.github.com>
2025-03-19 11:47:30 +0530

    Update app.py and Importing dependencies and packages

--- Backend/Gym-Trainer-Backend/app.py
+++ Backend/Gym-Trainer-Backend/app.py
@@ -1,1 +1,7 @@
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+from bson import ObjectId
+import os
+import json
+from datetime import datetime
