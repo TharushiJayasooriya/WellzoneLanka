0d2217045f125e467b1ecdaf5d3860e28edb5439
Disath Damsana <122661147+Disath2005@users.noreply.github.com>
2025-03-19 12:18:19 +0530

    Update app.py

--- Backend/Gym-Trainer-Backend/app.py
+++ Backend/Gym-Trainer-Backend/app.py
@@ -14,3 +14,9 @@ mongo_uri = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/wellzone')
 client = MongoClient(mongo_uri)
 db = client.get_database('wellzone')
 
+# Collections
+users = db.users
+appointments = db.appointments
+video_sessions = db.video_sessions
+
+
