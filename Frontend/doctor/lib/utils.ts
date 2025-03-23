ce4212c66d26ead9b0da0e657f1c4c06a23ef60f
Anjitha Vidyarathna <165659311+AnjithaSahashya@users.noreply.github.com>
2025-03-23 22:42:23 +0530

    refact: Refactor three frontend applications

--- Frontend/doctor/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-// This is the `cn` function that combines `clsx` and `twMerge`
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
